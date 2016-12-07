16041342e7f58431208e8d34aaad1905dc18c948
DanyOctrome <DanyOctrome@gmail.com>
2016-12-07 09:37:46 +0000

    initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BeeHappy.Global" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeeHappy.Startup))]
+namespace BeeHappy
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
